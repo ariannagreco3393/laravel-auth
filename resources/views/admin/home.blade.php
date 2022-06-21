0db2ffba5a6ce6311ef972afd737ca4addeb7865
ariannagreco3393 <a.greco3393@gmail.com>
2022-06-21 16:05:40 +0200

    add scss admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,18 +5,19 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}    {{ __('You are logged in!') }}</div>
-
+                <div class="card-header">{{ __('Dashboard') }} {{ __('You are logged in!') }}</div>
+                
                 <a class="btn btn-danger">prova</a>
+
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                 
+
                 </div>
             </div>
         </div>
